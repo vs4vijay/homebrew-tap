144af200d5fdde74dbd4ec12f69cc0f2e8281bb0
Vijay Soni (vs4vijay) <vs4vijay@gmail.com>
2020-03-30 16:26:41 +0530

    Brew formula update for vizix version v0.0.2

--- Formula/vizix.rb
+++ Formula/vizix.rb
@@ -2,16 +2,16 @@
 class Vizix < Formula
   desc "vizix - Software for fun and profit"
   homepage ""
-  version "0.0.1"
+  version "0.0.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/vs4vijay/vizix/releases/download/v0.0.1/vizix_0.0.1_mac_x86_64.tar.gz"
-    sha256 "2513e9200079c4f75690b6d5a8188eae96609bc209c13d75060af2dbba8ea554"
+    url "https://github.com/vs4vijay/vizix/releases/download/v0.0.2/vizix_0.0.2_mac_x86_64.tar.gz"
+    sha256 "fe485538bef4a13e38474b3178c0beec3cc767a0a4164dbd91f336b7ec440a9e"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/vs4vijay/vizix/releases/download/v0.0.1/vizix_0.0.1_linux_x86_64.tar.gz"
-      sha256 "b143132602f1a460316b546be2c174e14bd0e6d39676fe8bce796a2bf7467828"
+      url "https://github.com/vs4vijay/vizix/releases/download/v0.0.2/vizix_0.0.2_linux_x86_64.tar.gz"
+      sha256 "71f585893118d526b335b1895a778b0f660b9a98112c19303db29b3db2012960"
     end
   end
 
